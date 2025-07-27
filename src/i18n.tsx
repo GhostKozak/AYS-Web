488e8af9b2e2a5ca9a98758ffc03e5368d18a7ab
GhostKozak <codeodd3@gmail.com>
2025-07-27 09:05:00 +0300

    feat(i18n): add i18next for internationalization support

---
+++ src/i18n.tsx
@@ -0,0 +1,25 @@
+import i18n from 'i18next';
+import HttpBackend from 'i18next-http-backend';
+import LanguageDetector from 'i18next-browser-languagedetector';
+import { initReactI18next } from 'react-i18next';
+
+i18n
+  .use(HttpBackend)
+  .use(LanguageDetector)
+  .use(initReactI18next)
+  .init({
+    supportedLngs: ["en", "tr"],
+    fallbackLng: "en",
+    detection: {
+      order: ["navigator", "localStorage", "querystring", "cookie", "htmlTag"],
+      caches: ["localStorage", "cookie"],
+      lookupQuerystring: "lang",
+      lookupCookie: "lang",
+      lookupLocalStorage: "lang",
+    },
+    interpolation: {
+      escapeValue: false
+    },
+  });
+
+export default i18n;
