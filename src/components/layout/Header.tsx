40062655065b07dd2106867f7434b11ea3bc811f
GhostKozak <codeodd3@gmail.com>
2025-08-24 12:47:30 +0300

    feat: add menu items in Header

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,8 +1,10 @@
-import { Layout, Menu } from 'antd';
+import { Flex, Layout, Menu } from 'antd';
+import { Link } from 'react-router';
 
 function Header() {
   return (
     <Layout.Header>
+      <Flex justify='space-between'>
       <div className="logo">
         Ulusal Araç Yönetim Sistemi
       </div>
@@ -11,9 +13,29 @@ function Header() {
         mode="horizontal"
         defaultSelectedKeys={['2']}
         items={[
-          
+          {
+            key: 'dashboard',
+            label: <Link to="/dashboard">Dashboard</Link>
+          },
+          {
+            key: 'companies',
+            label: <Link to="/companies">Şirketler</Link>
+          },
+          {
+            key: 'drivers',
+            label: <Link to="/drivers">Sürücüler</Link>
+          },
+          {
+            key: 'vehicles',
+            label: <Link to="/vehicles">Araçlar</Link>
+          },
+          {
+            key: 'trips',
+            label: <Link to="/trips">Seferler</Link>
+          }
         ]}
       />
+      </Flex>
     </Layout.Header> 
   )
 }
