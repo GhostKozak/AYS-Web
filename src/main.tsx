ed28a9b3373eb0922179a35612eb6dbd589137df
GhostKozak <codeodd3@gmail.com>
2025-10-26 17:33:07 +0300

    chore: update dependencies and fix css

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,9 @@
-import { createRoot } from 'react-dom/client'
+import { createRoot } from "react-dom/client";
 import { RouterProvider } from "react-router";
 import AppRoutes from "./routes/AppRoutes";
-import './i18n';
+import "./i18n";
+import "./base.css";
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <RouterProvider router={AppRoutes()} />
-)
+);
